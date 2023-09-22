4745f05f77a8a5778b01b9500c5b200cf59673f8
MR.Roy <140273959+ChippalaPavanSai@users.noreply.github.com>
2023-09-23 00:19:48 +0530

    Add files via upload

---
+++ blog/partials/footer.php
@@ -0,0 +1,52 @@
+<footer>
+    <div class="container footer__container">
+        <article>
+            <h4>Useful Links</h4>
+            <ul>
+                <li><a href="https://theblackbucks.com/terms.html" target="_blank" style="text-decoration: none;">Terms and Conditions</a></li>
+                <li><a href="privacy-policy.html" target="_blank" style="text-decoration: none;">Privacy Policy</a></li>
+                <li><a href="cookie-policy.html" target="_blank" style="text-decoration: none;">Cookie Policy</a></li>
+                <li><a href="guidelines.html" target="_blank" style="text-decoration: none;">Security Guidelines</a></li>  
+                                           
+            </ul>
+        </article>
+        <article>
+            <h4>Contacts</h4>
+            <ul>
+                <li><a  href="https://"  target="_blank">Call me back</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Product Links</h4>
+            <ul>
+            </ul>
+        </article>
+        <article>
+            <h4>App Links</h4>
+            <ul>
+                <li><a href="#."><img src="https://theblackbucks.com/images/playstore.png" style="width:60%"></a></li>
+                <li><a href="#."><img src="https://theblackbucks.com/images/appstore.png" style="width:60%"></a></li>
+            </ul>
+        </article>
+    </div>
+    <div class="footer__socials">
+        <a href="https://youtube.com/egatortutorials" target="_blank"><i class="uil uil-youtube"></i></a>
+        <a href="https://facebook.com" target="_blank"><i class="uil uil-facebook-f"></i></a>
+        <a href="https://instagram.com" target="_blank"><i class="uil uil-instagram-alt"></i></a>
+        <a href="https://linkedin.com" target="_blank"><i class="uil uil-linkedin"></i></a>
+        <a href="https://twitter.com" target="_blank"><i class="uil uil-twitter"></i></a>
+    </div>
+    <div class="footer__copyright">
+        <small>                              
+            <div class=" copyright">
+                <p>Pavansai</p>
+            </div>
+        </small>
+    </div>
+</footer>
+
+
+<script src="<?= ROOT_URL ?>js/main.js"></script>
+</body>
+
+</html>
